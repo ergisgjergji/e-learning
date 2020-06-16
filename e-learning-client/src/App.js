e0354454f9593264c6d2a425011982b75442cfc0
Ergis Gjergji <ergisgjergji@gmail.com>
2020-06-16 19:28:12 +0200

    Installed React and all dependencies; did some cleanup

---
+++ e-learning-client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class App extends Component {
+
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
